66b1c58272b8e2aced1c68311e9d79bc511a28ce
Jesus <jqcelis092@gmail.com>
2026-08-26 14:24:27 -0400

    feat: configure Cloudinary and Multer storage integration for file uploads

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -2,7 +2,7 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
 
-// Configuración de Cloudinary usando variables de entorno reploy
+// Configuración de Cloudinary usando variables de entorno reploy re
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
